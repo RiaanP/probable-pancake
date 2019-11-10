0c9c4631a23feeda7ec00d800ef253c39b6d6aed
Shaun <emul8.mail@gmail.com>
2019-11-10 19:42:53 +0200

    fix/chore: Needed to save data(mongodb) on the server instead of just on my local machine...

--- api/DB.js
+++ api/DB.js
@@ -1,3 +1,4 @@
 module.exports = {
-    DB: 'mongodb://localhost:27017/dbleague'
+    //DB: 'mongodb://localhost:27017/dbleague'
+    DB: 'mongodb://emul8:leagueability2019@ds143221.mlab.com:43221/heroku_bxbgwxhz'
 };
