ffc73f359687c1f66452f385cfa740975ce71029
Shaun <emul8.mail@gmail.com>
2019-11-11 19:29:10 +0200

    Testing something

--- api/server.js
+++ api/server.js
@@ -17,7 +17,7 @@ mongoose.connect(mongolaburi, { useNewUrlParser: true, useUnifiedTopology: true 
     err => { console.log('Can not connect to the database'+ err)}
 );
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, './build')));
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -25,7 +25,7 @@ app.use(bodyParser.json());
 
 app.use('/person', personRoute);
 
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 app.listen(port, function(){
     console.log('Server is running on Port:',port);
 });
